3e23394632f7e02125606296132bc87ffad8f891
bovarysme <bovarysme@users.noreply.github.com>
2017-06-05 12:09:35 +0200

    Add requests to the requirements

--- setup.py
+++ setup.py
@@ -4,6 +4,9 @@ from setuptools import setup
 setup(
     name='fudge',
     packages=['fudge'],
+    install_requires=[
+        'requests',
+    ],
     entry_points={
         'console_scripts': ['fudge=fudge.cli:cli'],
     }
